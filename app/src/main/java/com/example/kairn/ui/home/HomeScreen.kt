7c7bbc37a61a78b6a28badc094fd15cf04f3c119
Pierre-Louis Rousseaux <pl74rou@gmail.com>
2026-03-04 15:11:39 +0100

    fix: remove invalid BlurMaskFilter import from HomeScreen

--- app/src/main/java/com/example/kairn/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/kairn/ui/home/HomeScreen.kt
@@ -45,7 +45,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.BlurMaskFilter
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.SolidColor
